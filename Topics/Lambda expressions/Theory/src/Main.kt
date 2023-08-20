3fa00d1501d4908c64bb9270449fe18b4628b942
denys <denamyte@gmail.com>
2023-08-21 01:48:55 +0300

    2023-08-20: 6 problems

---
+++ Topics/Lambda expressions/Theory/src/Main.kt
@@ -0,0 +1,23 @@
+import java.io.File
+import java.util.regex.Pattern
+
+// You can experiment here, it won’t be checked
+
+fun main(args: Array<String>) {
+//    val name = "/home/denys/Downloads/words_with_numbers.txt"
+//    val file = File(name)
+//    val re = Regex(".*[\\d].*")
+//    val withNumbers = file.readLines().filter { re.matches(it) }.size
+//    println(withNumbers)
+
+//    val file = File("/home/denys/Downloads/hyperskill/words_sequence.txt")
+//    val text = file.readText()
+//    println(text.length)
+//    val longest = text.split(Pattern.compile("\\s")).maxByOrNull { it.length }!!.length
+//    println(longest)
+
+//    val name = "/home/denys/Downloads/hyperskill/new.txt"
+//    val linesLength = File(name).length()
+//    val lines = File(name).readLines().size
+//    print("$linesLength $lines")
+}
